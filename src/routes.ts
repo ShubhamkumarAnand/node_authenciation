72aacd72aae2030a816049417e1b96f606a09c4e
imskanand <the.anand3099@gmail.com>
2022-02-20 09:41:10 +0530

    Added Logout Funcationality

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
     AuthenciatedUser,
     Login,
+    Logout,
     Refresh,
     Register,
 } from './controller/auth.controller';
@@ -11,4 +12,5 @@ export const routes = (router: Router) => {
     router.post('/api/login', Login);
     router.get('/api/user', AuthenciatedUser);
     router.post('/api/refresh', Refresh);
+    router.post('/api/logout', Logout);
 };
